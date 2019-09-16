998f1b89619139823edba2ad9a6b6a8b31a17e66
unknown <fsadler9@gmail.com>
2019-09-15 21:57:39 -0600

    Updates

---
+++ precache-manifest.d6afbce460bd93ba33bdfbe788bdfff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fa08ec5105c384a9a3a89ddb39e173",
+    "url": "/FrancescaSadler/pick-a-plant/index.html"
+  },
+  {
+    "revision": "8cc26fcee8ed88e3fb31",
+    "url": "/FrancescaSadler/pick-a-plant/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4ace9c70746e46096e0d",
+    "url": "/FrancescaSadler/pick-a-plant/static/css/main.b256c6a3.chunk.css"
+  },
+  {
+    "revision": "8cc26fcee8ed88e3fb31",
+    "url": "/FrancescaSadler/pick-a-plant/static/js/2.4bb347bc.chunk.js"
+  },
+  {
+    "revision": "4ace9c70746e46096e0d",
+    "url": "/FrancescaSadler/pick-a-plant/static/js/main.24a6d05e.chunk.js"
+  },
+  {
+    "revision": "1aaf7cbf47cbb808f334",
+    "url": "/FrancescaSadler/pick-a-plant/static/js/runtime~main.42dcfce6.js"
+  }
+]);
